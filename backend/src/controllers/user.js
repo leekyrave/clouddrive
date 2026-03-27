7cfc26b23e2e236ee1dee5c0c4f232f7192df577
Dmytro Pyvovarov <dmytro.pyovvarov@teliqon.io>
2026-03-27 22:56:00 +0100

    Revert "feature/create-user-registration; created user registration (#1)"
    This reverts commit bfc9d932cf7884eac4ee8b29eedcceb1d50dffba.

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -7,7 +7,7 @@ class UserController {
         const { username, password } = req.body;
 
         if (!username || !password) {
-            return res.status(401).json({
+            return res.status(400).json({
                 message: "All field are required"
             });
         }
@@ -15,7 +15,7 @@ class UserController {
         try {
             const { user, token } = await UserController.userService.registerUser(username, password);
 
-            return res.status(200).json({
+            return res.status(201).json({
                 message: 'User registered successfully',
                 user: {
                     id: user.id,
