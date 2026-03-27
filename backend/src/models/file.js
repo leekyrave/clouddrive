237a6b82342ea26f60dda91bc44f0d4e8058e3d3
Dmytro Pyvovarov <dmytro.pyovvarov@teliqon.io>
2026-03-27 22:43:03 +0100

    dimas commits

--- backend/src/models/file.js
+++ backend/src/models/file.js
@@ -5,7 +5,7 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class File extends Model {
     /**
-     * Helper method for defining associations.
+     * Helper method for definin some comments g associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
